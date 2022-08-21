1258d99fe8593aa60bcc24f87705bd3b6fd7b648
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-08-20 21:26:13 -0500

    Add files via upload

---
+++ j1-introduccion.js
@@ -0,0 +1,121 @@
+// Carcteristicas 
+
+// # lenguaje de alto nivel 
+// interpretado
+// Dinamico
+// Debilmente tipado
+// Multiparadigma
+// Debilmente tipado
+// Multiparadigma
+// Sensible a mayusculas y minusculas
+// No noecesitas los puntos y comas al final de cada linea 
+// sensible a mayusculas y minusculas 
+// no nesecitas los puntos y comas al final de cada linea
+
+// //Escritura de codigo 
+
+// una letra o 
+// un signo de dolar $ o 
+// un guion bajo _
+// nunca con numeros o caracteres especiales
+
+// Usar snake_name en nombres de archivos
+
+// mi_archivo javascript.js
+
+// usar UPPER_CASE:
+// contantes:
+
+//conts UNA_CONSTANTE = "SOY UNA CONSTANTE"
+
+
+//USAR UPPERCAMELCASE EN ------------------
+
+//clases 
+
+class serHumano{
+    constructor(nombre, genero){
+        this.nombre = nombre;
+        this.genero= generop;
+
+    }
+
+miNombresEs(){
+    return `Mi nombre es ${this.nombre}`;
+    }
+}
+
+//Usa lowwerCamelCase en:
+
+    const unObjeto = {
+        nomvre :"daniel",
+        apellido :"sjuares",
+    };
+
+
+// primitivos 
+let unaCadena = " Hola Mundo",
+unNumero = 19,
+unBoolean = true;
+
+//funciones 
+
+function holaMundo(nombre){
+    alert(`Hola mundo ${nombre}`);
+
+}
+holaMundo("Daniel");
+
+//Instancias:
+const ajax = new XMlhtppRequest(),
+dan = new serHumano("Jonathan", "Hombre");
+
+//palabras reservadas
+
+Abstract
+Boolean,break, byte 
+case, catch , char, class, const , continue 
+debugger, default , delete , do ,double
+else, enum , export , extends
+false, final , finally , float , for , function
+goto
+if,implements,import , int , instanceof, int , interface
+let, long 
+native, new , null
+package, private, protected , public 
+return
+ShadowRoot, static , super , switch , syncrinized 
+this, throw, throws, transiente , true , try , typeof
+var , volatile , void 
+while , with
+
+
+
+//ordenamiento de codigo
+
+importacion de modulos.
+declaracion de varibles 
+declaracion de funciones 
+ejecucion de codigo 
+
+
+
+//tipos de datos en java scriipt
+ string 
+ number 
+ boolean
+ null
+ undefined
+ Nan
+ //compuestos se accede a la referecnia del valor 
+
+ objects = {}
+ array = []
+ function (){}
+ class {}
+ etc 
+
+
+
+
+
