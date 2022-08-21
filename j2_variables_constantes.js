1df8f33ab5ff8647f857ff471ab014befe813058
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-08-20 21:52:31 -0500

    Add files via upload

---
+++ j2_variables_constantes.js
@@ -0,0 +1,43 @@
+solo debe de aver in nombre para cada variable unico
+las variables son identifacdores 
+puden ser cortos 
+reglasa generales:
+    los nombres pueden contener letras, digitos, guiones bajos y signos 
+
+    deben comnenzar con una letra 
+
+    los nombres deben comenzar con una letra 
+
+    los nombres tambien pueden comemzar con $ y _ (pero no lo usaremos en este tutorial)
+    
+    los nombres distinguen entre mayusculas y minusculas (y e Y son variables diferentes )
+
+
+variables
+
+let:
+
+nesecitamos declarar la palabra let
+    
+    let mensaje;
+
+    mensaje = "hola ";
+
+    alert(mensaje);
+
+constantes:
+
+    const nacimiento = '18.04.182'
+    nacimiento = '01.12.12'----------------- (nose puede reasignar la constante)
+
+
+constante mayuscula:
+
+    const COLOR_RED = "# F00";
+    const COLOR_GREEN = "# 0F0";
+    const COLOR_BLUE = "# 00F";
+    const COLOR_ORANGE = "# FF7F00 "
+
+Es mas fcil recordar un valor que esta dificl de escribir e interpretar en una constante mayuscula mas facil
+
+const pageLoadTime = / * tiempo tomado por una página web para cargar * /;
